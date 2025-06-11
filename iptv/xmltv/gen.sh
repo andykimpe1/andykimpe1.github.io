69efef6cdac8e8d0bad55a406644785544bb8116
andykimpe1 <andykimpe@gmail.com>
2025-06-12 00:13:37 +0200

    Update gen.sh

--- iptv/xmltv/gen.sh
+++ iptv/xmltv/gen.sh
@@ -2,9 +2,11 @@
 cd
 sudo apt-get update
 sudo apt upgrade -y
-sudo apt-get -y install mono-complete dos2unix git ssh net-tools wget nano
-rm -rf ~/.wg++ ~/WebGrabPlus_V5.1_install.tar.gz
-wget http://webgrabplus.com/sites/default/files/download/SW/V5.1.0/WebGrabPlus_V5.1_install.tar.gz
-tar -zxvf WebGrabPlus_V5.1_install.tar.gz
+sudo apt-get -y install dos2unix git ssh net-tools wget nano
+sudo apt-get -y install dotnet-runtime-7.0
+sudo apt-get -y install dotnet-runtime-8.0
+rm -rf ~/.wg++ ~/WebGrabPlus_V5.3_install.tar.gz
+wget https://webgrabplus.com/sites/default/files/download/SW/V5.3.0/WebGrabPlus_V5.3_install.tar.gz
+tar -zxvf WebGrabPlus_V5.3_install.tar.gz
 cd ~/.wg++
 ./install.sh
