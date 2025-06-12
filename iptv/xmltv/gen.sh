de8f12f8757731fceefa1669ca8bbd1cf7ac7cd3
andykimpe1 <andykimpe@gmail.com>
2025-06-12 09:11:34 +0200

    Update gen.sh

--- iptv/xmltv/gen.sh
+++ iptv/xmltv/gen.sh
@@ -10,51 +10,51 @@ wget https://webgrabplus.com/sites/default/files/download/SW/V5.3.0/WebGrabPlus_
 tar -zxvf WebGrabPlus_V5.3_install.tar.gz
 cd ~/.wg++
 ./install.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/TF1.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/TF1.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/France2.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/France2.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/France3.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/France3.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/France4.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/France4.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/France5.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/France5.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/M6.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/M6.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/Arte.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/Arte.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/LCP.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/LCP.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/W9.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/W9.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/TMC.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/TMC.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/TFX.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/TFX.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/GULLI.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/GULLI.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/BFMTV.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/BFMTV.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/CNEWS.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/CNEWS.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/LCI.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/LCI.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/CSTAR.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/CSTAR.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/T18.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/T18.xml -O WebGrab++.config.xml
 ./run.net.sh
-#wget https://andykimpe1.github.io/iptv/xmltv/config/Nova19.xml -O WebGrab++.config.xml
+#wget https://andykimpe1.github.io/iptv/xmltv/config/France/Nova19.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/TF1SERIESFILMS.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/TF1SERIESFILMS.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/LACHAINELEQUIPE.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/LACHAINELEQUIPE.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/6TER.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/6TER.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/RMCSTORY.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/RMCSTORY.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/RMCDECOUVERTE.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/RMCDECOUVERTE.xml -O WebGrab++.config.xml
 ./run.net.sh
-wget https://andykimpe1.github.io/iptv/xmltv/config/Cherie25.xml -O WebGrab++.config.xml
+wget https://andykimpe1.github.io/iptv/xmltv/config/France/Cherie25.xml -O WebGrab++.config.xml
 ./run.net.sh
