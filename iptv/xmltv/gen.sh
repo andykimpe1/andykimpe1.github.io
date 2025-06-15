60acd2de0a8ebf71d75faae41c9d93dfc85d4a5b
andykimpe1 <andykimpe@gmail.com>
2025-06-15 13:24:40 +0200

    Update gen.sh

--- iptv/xmltv/gen.sh
+++ iptv/xmltv/gen.sh
@@ -28,7 +28,8 @@ sed -i 's|<!DOCTYPE tv SYSTEM "xmltv.dtd">||g' TF1.xml
 sed -i 's|<tv source-info-url="https://api.telerama.fr" source-data-url="https://api.telerama.fr" generator-info-name="XMLTV" generator-info-url="http://mythtv-fr.org/">||g' TF1.xml
 sed -i 's|  <channel id="C192.api.telerama.fr">||g' TF1.xml
 sed -i 's|  <channel id="TF1.fr">||g' TF1.xml
-sed -i 's|    <display-name>FR| TF1 HD (TF1+)</display-name>||g' TF1.xml
+sed -i "s/    <display-name>FR|//g" TF1.xml
+sed -i 's| TF1 HD (TF1+)</display-name>||g' TF1.xml
 sed -i 's|    <icon src="https://focus.telerama.fr/100x100/0000/00/01/clear-192.png" />||g' TF1.xml
 sed -i 's|  </channel>||g' TF1.xml
 sed -i 's|</tv>||g' TF1.xml
