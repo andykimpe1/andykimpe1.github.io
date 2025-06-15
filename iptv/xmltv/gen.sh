983a2ee452a6ae767213b27ad86a7d5921553f62
andykimpe1 <andykimpe@gmail.com>
2025-06-15 12:43:45 +0200

    Update gen.sh

--- iptv/xmltv/gen.sh
+++ iptv/xmltv/gen.sh
@@ -8,17 +8,20 @@ if [ ! -f $FILE ]; then
    sudo apt-get -y install dos2unix git ssh net-tools wget nano unzip xmltv
    sudo apt-get -y install dotnet-runtime-7.0
    sudo apt-get -y install dotnet-runtime-8.0
+   sudo wget https://github.com/beavis69/tv_grab_fr_telerama/raw/refs/heads/master/tv_grab_fr_telerama -O /usr/bin/tv_grab_fr_telerama
+   sudo chmod +x /usr/bin/tv_grab_fr_telerama
    touch $FILE
 fi
 rm -rf $HOME/.wg++ $HOME/WebGrabPlus_V5.3_install.tar.gz
-sudo wget https://github.com/beavis69/tv_grab_fr_telerama/raw/refs/heads/master/tv_grab_fr_telerama -O /usr/bin/tv_grab_fr_telerama
-sudo chmod +x /usr/bin/tv_grab_fr_telerama
 rm -rf $HOME/andykimpe1.github.io $HOME/config.conf 
 git clone git@github.com:andykimpe1/andykimpe1.github.io.git
 cd $HOME/andykimpe1.github.io/iptv/xmltv/
-wget https://github.com/andykimpe1/andykimpe1.github.io/raw/refs/heads/main/iptv/xmltv/config/France/config.conf -O $HOME/config.conf
-rm -f guide.xml
-tv_grab_fr_telerama --config-file $HOME/config.conf --output guide.xml --days 7
+#wget https://github.com/andykimpe1/andykimpe1.github.io/raw/refs/heads/main/iptv/xmltv/config/France/config.conf -O $HOME/config.conf
+#rm -f guide.xml
+#tv_grab_fr_telerama --config-file $HOME/config.conf --output guide.xml --days 7
+wget https://github.com/andykimpe1/andykimpe1.github.io/raw/refs/heads/main/iptv/xmltv/config/France/tf1.conf -O $HOME/tf1.conf
+rm -f tf1.xml
+tv_grab_fr_telerama --config-file $HOME/tf1.conf --output tf1.xml --days 7
 sed -i "s|C192.api.telerama.fr|TF1.fr|g" guide.xml
 sed -i "s|C4.api.telerama.fr|France2.fr|g" guide.xml
 sed -i "s|C80.api.telerama.fr|France3.fr|g" guide.xml
