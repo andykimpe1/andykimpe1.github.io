d813ca29c371a447cd2a37ad9355e42e79409643
andykimpe1 <andykimpe@gmail.com>
2025-11-09 13:39:49 +0100

    Update fr-kodi.m3u.php

--- iptv/playlist/fr-kodi.m3u.php
+++ iptv/playlist/fr-kodi.m3u.php
@@ -3,7 +3,7 @@ header('content-type: audio/x-mpegurl');
 header('Content-Disposition: inline; filename="fr-kodi.m3u"');
 $molotovsing = "signature=20251110050000_a43ad7d819c1bfe203ae38cf6bc3796eb8f1fee9&aws.sessionId=ada4de35-dca8-4fea-9e03-fb07cb1e544d";
 $useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0";
-$date = "";
+$date = "20251109";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://andykimpe1.github.io/iptv/playlist/fr-kodi-test.m3u");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
